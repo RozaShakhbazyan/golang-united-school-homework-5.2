a712a421e03a70dc55cace028e1f2f064c96f875
r.shakhbazyan <roza.shakhbazyan-extern@invia.de>
2022-05-20 00:17:34 +0400

    Implemented constructor

--- cache.go
+++ cache.go
@@ -3,21 +3,47 @@ package cache
 import "time"
 
 type Cache struct {
+	key      string
+	value    string
+	expValue string
+	deadline time.Time
+	expired  bool
 }
 
-func NewCache() Cache {
-	return Cache{}
+func NewCache(key string,
+	value string,
+	expValue string,
+	deadline time.Time,
+	expired bool) Cache {
+	return Cache{key: key, value: value, expValue: expValue, deadline: deadline, expired: expired}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache Cache) Get(key string) (string, bool) {
+	if cache.key == key && cache.expired {
+		return cache.value, true
+	}
+	return "", false
 
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+
+	cache.key = key
+	cache.value = value
+	cache.expired = false
 }
 
-func (receiver) Keys() []string {
+func (cache Cache) Keys() []string {
+	if cache.expired {
+		return []string{}
+	}
+	return []string{cache.key}
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.key = key
+	cache.value = value
+	cache.deadline = deadline
+	cache.expired = deadline.After(time.Now())
+
 }
